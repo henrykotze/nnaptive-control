898bc478cc39877050ab4a46f1088b27fac66b38
henry-esl <19231865@sun.ac.za>
2019-02-05 09:29:00 +0200

    tranpose of DCM added and fixed typos

--- uav_model.py
+++ uav_model.py
@@ -3,17 +3,14 @@ import gym
 import numpy as np
 
 
-class drone(core.Env):
 '''
 sys_const: systems constants
 Type: np.array
 
 init_cond: initial conditions
-
-
-
 '''
-    __init__(self, sys_const, init_cond):
+class drone():
+    def __init__(self, sys_const, init_cond):
         self.mass = sys_const[1]
         self.Ixx = sys_const[2]
         self.Iyy = sys_const[3]
@@ -22,10 +19,13 @@ init_cond: initial conditions
 
 
 
-'''
-transform a velocity vector in the inertia axis system to the body axis
-'''
-    def DCM_transfrom(theta,phi,psi,x0,y0,z0):
-        return np.matrix([np.cos(psi)*np.cos(theta)], np.sin(psi)*np.cos(theta), -1*np.sin(theta);
-                            np.cos(psi)*np.sin(theta)*np.sin(phi)-np.sin(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.sin(phi))+np.cos(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.cos(phi)-np.cos(psi)*np.sin(phi);
-                            -np.sin(theta), np.cos(theta)*np.sin(phi), np.cos(theta)*np.cos(phi)])*np.matrix([x0;y0;z0])
+# Direct Cosine Matrix to execute axis system transformation through all three
+#Euler angles
+    def DCM_transfrom(theta,phi,psi):
+        return np.matrix([ [np.cos(psi)*np.cos(theta), np.sin(psi)*np.cos(theta), -1*np.sin(theta)], \
+                            [np.cos(psi)*np.sin(theta)*np.sin(phi)-np.sin(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.sin(phi)+np.cos(psi)*np.cos(phi), np.sin(psi)*np.sin(theta)*np.cos(phi)-np.cos(psi)*np.sin(phi)], \
+                            [-np.sin(theta), np.cos(theta)*np.sin(phi), np.cos(theta)*np.cos(phi)] ])
+
+# Transpose of the Direct Cosine Matrix
+    def transpose_DCM(theta,phi,psi):
+        return np.transpose(DCM_transfrom(theta,phi,psi))
