a5b981fa3289f7d1ec45d4aa9ca9fedbbc74506b
henry-esl <19231865@sun.ac.za>
2019-03-15 11:40:33 +0200

    improved logging of training

--- train_nn.py
+++ train_nn.py
@@ -26,16 +26,16 @@ parser = argparse.ArgumentParser(\
 
 parser.add_argument('-dataset', default='./datasets/', help='location of stored dataset, default: ./datasets')
 parser.add_argument('-epochs', default=1, help='Number of Epochs, default: 1')
-parser.add_argument('-mdl_loc', default='./', help='Location to save model: ./')
 parser.add_argument('-mdl_name', default='nn_mdl', help='Name of model, default: nn_mdl')
 parser.add_argument('-reg_w', default='0', help='Regularization of weight, default: 0')
 parser.add_argument('-lr', default='0', help='learning rate, default: 0')
 parser.add_argument('-valset', default='./datasets/', help='location of validation set, default: ./datasets')
 parser.add_argument('-name_train', default='dataset', help='Name Of Training Datasets: ')
 parser.add_argument('-name_val', default='validation_data', help='Name Of Validation Datasets: ')
+parser.add_argument('-log_loc', default='./training_results/', help='Location of logs and checkpoints')
 
 
-
+now = datetime.now()
 
 
 args = parser.parse_args()
@@ -43,21 +43,34 @@ args = parser.parse_args()
 dir = vars(args)['dataset']
 epochs = int(vars(args)['epochs'])
 mdl_name = str(vars(args)['mdl_name'])
-mdl_loc = str(vars(args)['mdl_loc'])
 weight_reg = float(vars(args)['reg_w'])
 learning_rate = float(vars(args)['lr'])
 validation_dir = vars(args)['valset']
 training_dataset = vars(args)['name_train']
 validation_dataset = vars(args)['name_val']
 
+# Directory containing all different models trained with all their logging
+# info
+log_dir = vars(args)['log_loc']
 
+
+# directory with the name of the training session
+model_logging_dir = log_dir + '/'+ mdl_name + now.strftime("_%Y%m%d_%H%M%S")
+os.mkdir(model_logging_dir)
+
+# location of training dataset
 dataset_path = dir + '/' + training_dataset
+# location of validation datset
 validation_path = validation_dir + '/' + validation_dataset
-# Base name for data files:
-# filename='./learning_data/response-0.npz'
-# data_directory='./learning_data/'
 
-import keras.backend as K
+
+# Directory containing the logs of the session
+tensorboard_logging = model_logging_dir+"/tf_logs"
+
+checkpoint_path = model_logging_dir+"/checkpoints" + '/cp-{epoch:04d}.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_path)
+os.mkdir(checkpoint_dir)
+
 
 def precision(y_true,y_pred):
     return K.abs((y_true-y_pred)/y_true)
@@ -78,10 +91,9 @@ def build_model(dataset):
 
     model = keras.Sequential([
     # layers.Flatten(input_shape=(4,)),\
-    layers.Dense(3,kernel_regularizer=keras.regularizers.l2(weight_reg),input_shape=dataset.output_shapes[0] ), \
+    layers.Dense(5,kernel_regularizer=keras.regularizers.l2(weight_reg),input_shape=dataset.output_shapes[0] ), \
     layers.ReLU(),\
-    # layers.Dropout(0.2),\
-    # layers.Dense(5,kernel_regularizer=keras.regularizers.l2(weight_reg)),\
+    # layers.Dense(2,kernel_regularizer=keras.regularizers.l2(weight_reg)),\
     # layers.ReLU(),\
     # layers.Dense(20,kernel_regularizer=keras.regularizers.l2(weight_reg)),\
     # layers.ReLU(),\
@@ -134,9 +146,9 @@ def plot_history(history):
 def loadData(dir):
     # in the directory, dir, determine how many *.npz files it contains
     with open(str(dir),'rb') as filen:
-        print('=======================================')
+        print('==============================================================')
         print('Loading dataset from: ' ,str(dir))
-        print('=======================================')
+        print('==============================================================\n')
         features,labels = pickle.load(filen)
 
     # each row of `features` corresponds to the same row as `labels`.
@@ -174,22 +186,34 @@ if __name__ == '__main__':
 
     model.summary()
 
-    with open(str(mdl_loc + '/'+mdl_name+'_readme'),'wb+') as filen:
-        print('Saving training info to:', str(mdl_loc+'/'+mdl_name))
-        pickle.dump([epochs,mdl_name,mdl_loc,weight_reg,learning_rate],filen)
+
+
+
+
+    with open(str(model_logging_dir + '/'+'readme'),'wb+') as filen:
+        print('Saving training info to:', str(model_logging_dir + '/'+'readme'))
+        pickle.dump([epochs,mdl_name,weight_reg,learning_rate],filen)
 
     filen.close()
 
-    now = datetime.now()
-    logdir = "./tf_logs" + now.strftime("%Y%m%d-%H%M%S")
-    tbCallBack = keras.callbacks.TensorBoard(log_dir=logdir,\
+    # Callback for Checkpoint
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 monitor='val_loss',
+                                                 period=1,
+                                                 save_best_only=True,
+                                                 verbose=1)
+    # Callback for TensorBoard
+    tbCallBack = keras.callbacks.TensorBoard(log_dir=tensorboard_logging,\
                                             histogram_freq=1,\
                                             write_graph=True,\
                                             write_images=True,\
                                             write_grads=True)
 
+
+
+    # Learning of Model
     history = model.fit(features, labels, epochs=epochs, \
-    validation_data=(features_val,labels_val), verbose=1, callbacks=[tbCallBack])
+    validation_data=(features_val,labels_val), verbose=1, callbacks=[tbCallBack,cp_callback])
     # validation_split=0.1, verbose=1, callbacks=[TrainValTensorBoard(log_dir=logdir, write_graph=False)])
     # validation_data=(features_val,labels_val),verbose=1,callbacks=[TrainValTensorBoard(log_dir=logdir, write_graph=False)])
 
