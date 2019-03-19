c47f2ed766fc490b72adfe702672c6ce80323d5e
henry-esl <19231865@sun.ac.za>
2019-03-19 08:47:16 +0200

    added noisy pendulum

--- single_pendulum_v2.py
+++ single_pendulum_v2.py
@@ -12,3 +12,17 @@ class pendulum(second_order):
 
     def update_ydotdot(self):
         self.ydotdot = self.input - 2*self.zeta*self.wn*self.ydot - np.power((self.wn),2)*np.sin(self.y)
+
+
+class noisy_pendulum(pendulum):
+    def __init__(self):
+        super().__init__(self,wn,zeta,time_step=0.001,input=0, ydotdot = 0, ydot = 0, y = 0)
+
+    def step(self):
+        self.update_ydotdot()
+
+
+        self.ydot += self.integration(self.ydotdot)[0]
+        self.y += self.integration(self.ydot)[0] # + noise
+
+        self.t += self.dt
